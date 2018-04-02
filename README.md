bef9d8f3d0d17ae727de24d1860cfac849565c82
Tom Ross <tom@umpox.com>
2018-04-02 22:16:29 +0100

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# zero-width-detection
-A project to demonstrate the vulnerabilities of copying text that may have zero-width characters inserted into it.
--- src/index.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable react/jsx-filename-extension */
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
